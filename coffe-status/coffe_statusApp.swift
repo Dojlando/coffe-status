d460829c4dd23ec810d235fd1d14e304af00ab92
Daniel Jansson <daniel.jansson@swace.se>
2024-12-01 21:57:50 +0100

    Coffe state

--- coffe-status/coffe_statusApp.swift
+++ coffe-status/coffe_statusApp.swift
@@ -7,11 +7,67 @@
 
 import SwiftUI
 
+struct CoffeState {
+    var cupsLeft: Int;
+    var state: String;
+    var updated: Date;
+}
+
+class CoffeStateStore: ObservableObject {
+    @Published var coffeState: CoffeState = .init(cupsLeft: 0, state: "", updated: Date())
+    var timer: Timer?
+    
+    init() {
+        Task {
+            await self.setCoffeState(state: await self.getCurrentCoffeState())
+        }
+        timer = Timer.scheduledTimer(withTimeInterval: 30.0, repeats: true) { _ in
+            Task {
+                await self.setCoffeState(state: await self.getCurrentCoffeState())
+            }
+        }
+    }
+    deinit {
+        timer?.invalidate()
+    }
+    
+    func setCoffeState (state: CoffeState) async {
+        await MainActor.run {
+            coffeState = state
+        }
+    }
+    
+    func getCurrentCoffeState() async -> CoffeState {
+        do {
+            let url = URL(string: "https://scalenet.swace.cloud/api/status")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let json = try JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+            
+            return CoffeState(cupsLeft: json["cupsLeft"]! as! Int, state: json["state"]! as! String, updated: Date())
+            // return  CoffeState (cupsLeft: Int(json["cupsLeft"]!))
+        }
+        catch {
+            print(error)
+            return CoffeState(cupsLeft: -1, state: "Error", updated: Date())
+        }
+    }
+
+}
+
 @main
-struct coffe_statusApp: App {
+struct CoffeStatusApp: App {
+    @StateObject var store = CoffeStateStore()
+    
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        MenuBarExtra {
+            Text("Coffe Status: \(store.coffeState.state)");
+            Text("Updated: \(String(describing: store.coffeState.updated))");
+            Button("Quit") {
+                NSApplication.shared.terminate(nil)
+            }.keyboardShortcut("q")
+        } label: {
+            Image(systemName: "cup.and.saucer.fill")
+            Text("\(store.coffeState.cupsLeft)")
         }
     }
 }
